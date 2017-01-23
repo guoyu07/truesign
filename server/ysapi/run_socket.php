122a309a80a83e034fc475df466d37bcadf52807
ql_os <root@iamsee.com>
2017-01-23 16:49:59 +0800

    mm

--- server/ysapi/run_socket.php
+++ server/ysapi/run_socket.php
@@ -2,15 +2,15 @@
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
 try {
-	define('IS_CLI', PHP_SAPI==='cli');
-	if(!IS_CLI)exit();
-	define('APPLICATION_NAME', 'api');
-	define('APPLICATION_PATH', realpath(__DIR__));
-	include(sprintf('%s/service/service_%s.php', APPLICATION_PATH, APPLICATION_NAME));
-	include(sprintf('%s/service/websocket_api.php', APPLICATION_PATH));
+    define('IS_CLI', PHP_SAPI==='cli');
+    if(!IS_CLI)exit();
+    define('APPLICATION_NAME', 'api');
+    define('APPLICATION_PATH', realpath(__DIR__));
+    include(sprintf('%s/service/service_%s.php', APPLICATION_PATH, APPLICATION_NAME));
+    include(sprintf('%s/service/websocket_api.php', APPLICATION_PATH));
 
     $socket_server = new socket_server('socket');
     $socket_server->run();
 }catch (Exception $e){
-	die('run-ERROR: '.$e->getMessage().PHP_EOL);
+    die('run-ERROR: '.$e->getMessage().PHP_EOL);
 }
