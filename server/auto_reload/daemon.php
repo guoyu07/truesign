d366701ad14b0106005c7faff9cdedcf660cc026
DESKTOP-831MTMB\ql-win <root@iamsee.com>
2017-04-18 22:35:21 +0800

    mm

--- server/auto_reload/daemon.php
+++ server/auto_reload/daemon.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__.'/src/Swoole/ToolKit/AutoReload.php';
 
-$kit = new Swoole\ToolKit\AutoReload(2914);
-$kit->watch(__DIR__.'../link_swoole');
+$kit = new Swoole\ToolKit\AutoReload(6742);
+$kit->watch(__DIR__.'/../link_swoole');
 $kit->addFileType('.php');
 $kit->run();
