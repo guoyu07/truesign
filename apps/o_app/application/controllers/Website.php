0977aab95c8394948ddcb68105d1eb469633ee3e
DESKTOP-831MTMB\ql-win <root@iamsee.com>
2017-04-19 01:38:45 +0800

    网站初始化

--- apps/o_app/application/controllers/Website.php
+++ apps/o_app/application/controllers/Website.php
@@ -15,9 +15,9 @@ class WebSiteController extends  OAppBaseController
     {
         $params = $this->getParams(array('username', 'pass', 'email'), array('look_for', 'ip'));
         $doDao = new \Royal\Data\DAO(new \Truesign\Adapter\Apps\appWebSiteAdapter());
-        $db_checkUsername_response = $doDao->readSpecified(array('username' => $params['username']));
-        $db_checkEmail_response = $doDao->readSpecified(array('email' => $params['email']));
-        $db_Login_response = $doDao->readSpecified(array('username' => $params['username'], 'pass' => $params['pass']));
+        $db_checkUsername_response = $doDao->readSpecified(array('username' => $params['username']),array());
+        $db_checkEmail_response = $doDao->readSpecified(array('email' => $params['email']),array());
+        $db_Login_response = $doDao->readSpecified(array('username' => $params['username'], 'pass' => $params['pass']),array());
         $db_reg_response = $doDao->create($params);
         $this->setResponseBody($db_checkUsername_response);
     }
