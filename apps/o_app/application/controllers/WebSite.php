9393959e1a22ef0950bc7768ea209aadf0c62cdc
DESKTOP-831MTMB\ql-win <root@iamsee.com>
2017-04-19 01:05:58 +0800

    网站初始化

---
+++ apps/o_app/application/controllers/WebSite.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ql-win
+ * Date: 2017/3/10
+ * Time: 13:20
+ */
+class WebSiteController extends  OAppBaseController
+{
+    public function indexAction(){
+
+        echo 'website';
+    }
+    public function regOrLogin()
+    {
+        $params = $this->getParams(array('username', 'pass', 'email'), array('look_for', 'ip'));
+        $doDao = new \Royal\Data\DAO(new \Truesign\Adapter\Apps\appWebSiteAdapter());
+        $db_checkUsername_response = $doDao->readSpecified(array('username' => $params['username']));
+        $db_checkEmail_response = $doDao->readSpecified(array('email' => $params['email']));
+        $db_Login_response = $doDao->readSpecified(array('username' => $params['username'], 'pass' => $params['pass']));
+        $db_reg_response = $doDao->create($params);
+        $this->setResponseBody($db_checkUsername_response);
+    }
+
+}
