80cad545aa8ecc8d0351bc6fe4c64ebe71e224db
ql_os <root@iamsee.com>
2017-04-24 07:56:38 +0800

    composer 安装 wechat sdk

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Pimple' => array($vendorDir . '/pimple/pimple/src'),
     'Kafka\\' => array($vendorDir . '/nmred/kafka-php/src'),
     'Illuminate\\Support' => array($vendorDir . '/illuminate/support'),
     'Illuminate\\Events' => array($vendorDir . '/illuminate/events'),
