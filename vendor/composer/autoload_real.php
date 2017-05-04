0eeba38e0d6a2da74a84d3ccdf61f386a4761b05
ql_os <root@iamsee.com>
2017-05-04 18:48:05 +0800

    m

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit9a3ede72af7ff81950ab8fc3f5f30544
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit9a3ede72af7ff81950ab8fc3f5f30544', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
